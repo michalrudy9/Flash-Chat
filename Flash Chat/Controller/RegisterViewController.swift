2c61647406d98ad18ab0cf1d4d0fc423a843522f
michalrudy9 <michallrudy9@gmail.com>
2023-12-01 15:12:22 +0100

    Added register new user feature

--- Flash Chat/Controller/RegisterViewController.swift
+++ Flash Chat/Controller/RegisterViewController.swift
@@ -6,13 +6,28 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class RegisterViewController: UIViewController {
 
+    @IBOutlet weak var emailTextfield: UITextField!
+    @IBOutlet weak var passwordTextfield: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func registerPressed(_ sender: UIButton) {
+        if let email = emailTextfield.text, let password = passwordTextfield.text {
+            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                if let e = error {
+                    print(e.localizedDescription)
+                } else {
+                    self.performSegue(withIdentifier: "RegisterToChat", sender: self)
+                }
+            }
+        }
+    }
 }
