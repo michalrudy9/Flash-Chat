f9e9bd2106f50ce66d5b4cca85160edaf73dad80
michalrudy9 <michallrudy9@gmail.com>
2023-12-02 21:11:13 +0100

    Added user logout and configured navigation bar

--- Flash Chat/Controller/ChatViewController.swift
+++ Flash Chat/Controller/ChatViewController.swift
@@ -6,24 +6,22 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ChatViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        title = "⚡️FlashChat"
+        navigationItem.hidesBackButton = true
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func logOutPressed(_ sender: UIBarButtonItem) {
+        do {
+            try Auth.auth().signOut()
+            navigationController?.popToRootViewController(animated: true)
+        } catch  let signOutError as NSError {
+            print("Error signing out: %@", signOutError)
+        }
     }
-    */
-
 }
