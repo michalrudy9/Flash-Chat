4cee2a8efd8f1a5f04f0ea057f750e72c8008c20
michalrudy9 <michallrudy9@gmail.com>
2023-12-03 15:11:42 +0100

    Provided file with constants in app

---
+++ Flash Chat/Constants.swift
@@ -0,0 +1,28 @@
+//
+//  Constants.swift
+//  Flash Chat
+//
+//  Created by Michał Rudy on 03/12/2023.
+//
+
+struct K {
+    static let appName = "⚡️FlashChat"
+    static let cellIdentifier = "ReusableCell"
+    static let cellNibName = "MessageCell"
+    static let registerSegue = "RegisterToChat"
+    static let loginSegue = "LoginToChat"
+    
+    struct BrandColors {
+        static let purple = "BrandPurple"
+        static let lightPurple = "BrandLightPurple"
+        static let blue = "BrandBlue"
+        static let lighBlue = "BrandLightBlue"
+    }
+    
+    struct FStore {
+        static let collectionName = "messages"
+        static let senderField = "sender"
+        static let bodyField = "body"
+        static let dateField = "date"
+    }
+}
