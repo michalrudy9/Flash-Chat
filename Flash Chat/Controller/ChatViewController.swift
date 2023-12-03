e8f0b250fec5469ecd1f7bc650ea428e5d757a9b
michalrudy9 <michallrudy9@gmail.com>
2023-12-03 19:25:45 +0100

    Configured the display of user message in TableView

--- Flash Chat/Controller/ChatViewController.swift
+++ Flash Chat/Controller/ChatViewController.swift
@@ -10,10 +10,18 @@ import FirebaseAuth
 
 class ChatViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    var messages: [Message] = [
+        Message(sender: "test@poczt.com", body: "Hey!"),
+        Message(sender: "test1@poczt.com", body: "Hello!"),
+        Message(sender: "test@poczt.com", body: "What's up?"),
+    ]
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
         title = K.appName
         navigationItem.hidesBackButton = true
+        tableView.register(UINib(nibName: K.cellNibName, bundle: nil), forCellReuseIdentifier: K.cellIdentifier)
     }
     
     @IBAction func logOutPressed(_ sender: UIBarButtonItem) {
@@ -23,5 +31,17 @@ class ChatViewController: UIViewController {
         } catch  let signOutError as NSError {
             print("Error signing out: %@", signOutError)
         }
+    }
+}
+
+extension ChatViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath) as! MessageCell
+        cell.label.text = messages[indexPath.row].body
+        return cell
     }
 }
---
+++ Flash Chat/Model/Message.swift
@@ -0,0 +1,13 @@
+//
+//  Message.swift
+//  Flash Chat
+//
+//  Created by Michał Rudy on 03/12/2023.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
