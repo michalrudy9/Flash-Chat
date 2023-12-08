a9677555cea87a95c7201ef4145c235e23dfef6b
michalrudy9 <michallrudy9@gmail.com>
2023-12-08 11:01:47 +0100

    Added ui and ux improvments

--- Flash Chat/Controller/WelcomeViewController.swift
+++ Flash Chat/Controller/WelcomeViewController.swift
@@ -12,10 +12,28 @@ class WelcomeViewController: UIViewController {
 
     @IBOutlet weak var titleLabel: CLTypingLabel!
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.isNavigationBarHidden = false
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         titleLabel.text = K.appName
+        
+        //Solution for not working bar tint color
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = UIColor(named: K.BrandColors.blue)
+        appearance.titleTextAttributes = [.foregroundColor: UIColor.white, .font: UIFont.systemFont(ofSize: 25, weight: .black)]
+        navigationController?.navigationBar.standardAppearance = appearance
+        navigationController?.navigationBar.scrollEdgeAppearance = navigationController?.navigationBar.standardAppearance
     }
 
 
